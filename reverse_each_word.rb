61248978853d88a0718259fd8473007c8d3c50ce
cedric e goddard <8ceg6@comcast.net>
2019-12-13 15:24:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  string = string.split
-  reversed = []
-  string.each do |phrase|
-    reversed << phrase.reverse 
-  end
-    reversed.join(" ")
-end  
+# def reverse_each_word(string)
+#   string = string.split
+#   reversed = []
+#   string.each do |phrase|
+#     reversed << phrase.reverse 
+#   end
+#     reversed.join(" ")
+# end  
 
 def reverse_each_word(string2)
     string2 = string2.split
